b96b4fad2cc35dd3ed2eeddc7457b50bb50bc7ec
19wh1a1238 <69349504+19wh1a1238@users.noreply.github.com>
2023-06-15 11:46:09 +0530

    Add files via upload

---
+++ CLIENT/SRC/Core/fixedPrices.js
@@ -0,0 +1,78 @@
+export const prices = [{
+        _id: 0,
+        name: 'Any',
+        array: [],
+    },
+    {
+        _id: 1,
+        name: '0 to ₹90000',
+        array: [0, 90000],
+    },
+    {
+        _id: 2,
+        name: '₹100000 to ₹190000',
+        array: [100000, 190000],
+    },
+    {
+        _id: 3,
+        name: '₹200000 to ₹290000',
+        array: [200000, 290000],
+    },
+    {
+        _id: 4,
+        name: '₹300000 to ₹390000',
+        array: [300000, 390000],
+    },
+    {
+        _id: 5,
+        name: 'More than 400000',
+        array: [400000, 990000],
+    },
+];
+
+export const departments = [{
+        _id: 1,
+        name: 'AI & ML',
+        value: 'AI & ML',
+    },
+    {
+        _id: 2,
+        name: 'CSE',
+        value: 'CSE',
+    },
+    {
+        _id: 3,
+        name: 'ECE',
+        value: 'ECE',
+    },
+    {
+        _id: 4,
+        name: 'EEE',
+        value: 'EEE',
+    },
+    {
+        _id: 5,
+        name: 'IT',
+        value: 'IT',
+    },
+    {
+        _id: 6,
+        name: 'Civil',
+        value: 'Civil',
+    },
+    {
+        _id: 7,
+        name: 'Mechanical',
+        value: 'Mechanical',
+    },
+    {
+        _id: 8,
+        name: 'Data science',
+        value: 'Data science',
+    },
+    {
+        _id: 9,
+        name: 'Cyber security',
+        value: 'Cyber security',
+    },
+];
